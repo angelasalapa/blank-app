646e5f730da3cd3dba2d3d3404a02695fd41caaf
Angela Salapa <angelasalapa@gmail.com>
2026-04-23 07:15:35 +0000

    new folder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Angela's new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.columns(spec, *, gap="small", vertical_alignment="top", border=False, width="stretch")
